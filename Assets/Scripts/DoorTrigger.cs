d8dd45ee65df79d02a3891eb1a01e9210c2945f5
flolewis <55874067+flolewis@users.noreply.github.com>
2020-12-16 09:26:35 +0800

    Finished Episode 1

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public GameObject indicator;
+    public Animator animator;
+    bool isOpen;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            indicator.gameObject.SetActive(true);
+            Debug.Log("Player Entered Range");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            indicator.gameObject.SetActive(false);
+            Debug.Log("Player Exited Range");
+        }
+    }
+
+    private void DoorAnimTrigger(){
+        isOpen = !isOpen;
+        if(indicator.activeInHierarchy && Input.GetKey(KeyCode.F)){
+            animator.SetBool("Open",isOpen);
+        }
+    }
+    private void Update() {
+        DoorAnimTrigger();
+    }
+}
